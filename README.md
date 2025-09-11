7aaff2a1fec3e0e80c7fd6a19be0a357661c4229
Disty0 <disty@disty.xyz>
2025-09-11 15:43:59 +0300

    Add training examples and cleanup

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 For more info, please check out SD.Next SDNQ wiki page: https://github.com/vladmandic/sdnext/wiki/SDNQ-Quantization  
 
+
 Example quantization config code for Diffusers and Transformers libraries:  
 
 ```py
@@ -24,8 +25,36 @@ sdnq_config = SDNQConfig(
     dequantize_fp32=False,
     non_blocking=False,
     quantization_device="cuda",
-    return_device="cpu",
+    return_device="cuda",
     modules_to_not_convert=["correction_coefs", "prediction_coefs", "lm_head", "embedding_projection"],
     modules_dtype_dict={"int8": ["lm_head"]},
 )
+
+model = AutoModel.from_pretrained(model_id, quantization_config=sdnq_config)
+```
+
+
+Example code for quantized training:  
+
+```py
+from sdnq_training import apply_sdnq_to_module
+
+model = apply_sdnq_to_module(
+    model,
+    weights_dtype="int8",
+    use_grad_ckpt=True,
+    use_quantized_matmul=True,
+    use_static_quantization=True,
+    use_stochastic_quantization=True,
+    modules_to_not_convert=["correction_coefs", "prediction_coefs", "lm_head", "embedding_projection"],
+)
+```
+
+
+Example code for quantized optimizer states:  
+
+```py
+from sdnq_training import SDNQTensor
+
+state["exp_avg"] = SDNQTensor.from_float(torch.zeros_like(p).add_(torch.finfo(p.dtype).eps), qtype="int8", sr=True)
 ```
--- src/sdnq_training/__init__.py
+++ src/sdnq_training/__init__.py
@@ -6,12 +6,17 @@ from .dequantizer import SDNQTensor
 
 
 @torch.no_grad()
-def apply_sdnq_to_module(model, config: dict, modules_to_not_convert: List[str] = []):
-    dtype = config["quantized_matmul_dtype"]
-    use_grad_ckpt = config["gradient_checkpointing"]
-    use_quantized_matmul = config["use_quantized_matmul"]
-    use_static_quantization = config["use_static_quantization"]
-    use_sr = config["use_stochastic_quantization"]
+def apply_sdnq_to_module(
+    model: torch.nn.Module,
+    weights_dtype: str = "int8",
+    use_grad_ckpt: bool = True,
+    use_quantized_matmul: bool = True,
+    use_static_quantization: bool = True,
+    use_stochastic_quantization: bool = True,
+    modules_to_not_convert: List[str] = None,
+):
+    if modules_to_not_convert is None:
+        modules_to_not_convert = []
     quantized_forward = None
 
     has_children = list(model.children())
@@ -25,7 +30,7 @@ def apply_sdnq_to_module(model, config: dict, modules_to_not_convert: List[str] 
         if module.__class__.__name__ == "Linear" and hasattr(module, "weight") and module.weight is not None:
             output_channel_size, channel_size = module.weight.shape
             if channel_size >= 32 and output_channel_size >= 32:
-                if dtype == "int8":
+                if weights_dtype == "int8":
                     use_quantized_matmul = use_quantized_matmul and output_channel_size % 8 == 0 and channel_size % 8 == 0
                     if use_quantized_matmul:
                         if use_grad_ckpt:
@@ -45,7 +50,7 @@ def apply_sdnq_to_module(model, config: dict, modules_to_not_convert: List[str] 
                     elif use_static_quantization:
                         from .layers.linear.forward import quantized_linear_forward
                         quantized_forward = quantized_linear_forward
-                elif dtype == "fp8":
+                elif weights_dtype == "fp8":
                     use_quantized_matmul = use_quantized_matmul and output_channel_size % 16 == 0 and channel_size % 16 == 0
                     if use_quantized_matmul:
                         if use_grad_ckpt:
@@ -66,15 +71,23 @@ def apply_sdnq_to_module(model, config: dict, modules_to_not_convert: List[str] 
                         from .layers.linear.forward import quantized_linear_forward
                         quantized_forward = quantized_linear_forward
                 else:
-                    raise NotImplementedError(f'Quantization type {dtype} is not implemented')
+                    raise NotImplementedError(f'Quantization type {weights_dtype} is not implemented')
 
                 if quantized_forward is not None:
                     module.forward = quantized_forward
                     module.forward = module.forward.__get__(module, module.__class__)
                     if use_static_quantization:
-                        module.weight = torch.nn.Parameter(SDNQTensor.from_float(module.weight, qtype=dtype, sr=use_sr), requires_grad=module.weight.requires_grad)
+                        module.weight = torch.nn.Parameter(SDNQTensor.from_float(module.weight, qtype=weights_dtype, sr=use_stochastic_quantization), requires_grad=module.weight.requires_grad)
 
-        module = apply_sdnq_to_module(module, config, modules_to_not_convert=modules_to_not_convert)
+        module = apply_sdnq_to_module(
+            module,
+            weights_dtype=weights_dtype,
+            use_grad_ckpt=use_grad_ckpt,
+            use_quantized_matmul=use_quantized_matmul,
+            use_static_quantization=use_static_quantization,
+            use_stochastic_quantization=use_stochastic_quantization,
+            modules_to_not_convert=modules_to_not_convert,
+        )
     return model
 
 
