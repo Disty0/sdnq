8bbc7471fa457bd599fd605fe2f78413ff31f91f
Disty0 <disty@disty.xyz>
2025-10-12 01:08:05 +0300

    Expose SDNQOptimizer

--- src/sdnq/optim/__init__.py
+++ src/sdnq/optim/__init__.py
@@ -1,3 +1,4 @@
+from .optimizer import SDNQOptimizer
 from .adafactor import Adafactor
 from .adamw import AdamW
 from .came import CAME
@@ -6,6 +7,7 @@ from .muon import Muon
 
 
 __all__ = [
+    "SDNQOptimizer",
     "Adafactor",
     "AdamW",
     "CAME",
